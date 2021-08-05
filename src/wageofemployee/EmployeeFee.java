0e78e59b8db985e27dba32ee01b3a451fbac593b
krishna319 <km83843@gmail.com>
2021-08-05 18:02:44 +0530

    added:employee wage of part time

--- src/wageofemployee/EmployeeFee.java
+++ src/wageofemployee/EmployeeFee.java
@@ -4,16 +4,20 @@ public class EmployeeFee {
 	public static void main(String[] args) {
 
 		int IS_PRESENT = 1;
+		int IS_PART_TIME_PRESENT =2;
 		int WAGE_PER_HOUR = 20;
 		int FULL_TIME = 8;
+		int PART_TIME = 4;
 		int dailyWage = 0;
 		double empCheck = Math.floor(Math.random() * 10) % 3;
 		if (empCheck == IS_PRESENT) {
 			System.out.println("Employee is present");
 			dailyWage = WAGE_PER_HOUR * FULL_TIME;
-		}
+		} else if (empCheck == IS_PART_TIME_PRESENT) {
+			System.out.println("Employe is part time present");
+			dailyWage = WAGE_PER_HOUR * PART_TIME;
 
-		else {
+		} else {
 			System.out.println("Employee is absent");
 		}
 		System.out.println("Emp Wage: " + dailyWage);
