804bb9ddfef4585677aa132d51600a24d1c873de
krishna319 <km83843@gmail.com>
2021-08-05 17:57:30 +0530

    added:employeewage of fulltime

--- src/wageofemployee/EmployeeFee.java
+++ src/wageofemployee/EmployeeFee.java
@@ -1,14 +1,22 @@
 package wageofemployee;
 
 public class EmployeeFee {
-
 	public static void main(String[] args) {
-		int IS_FULL_TIME = 1;
-		double empCheck = Math.floor(Math.random() * 10) % 2;
-		if (empCheck == IS_FULL_TIME)
-			System.out.println("Employee is Present");
-		else
-			System.out.println("Employee is Absent");
+
+		int IS_PRESENT = 1;
+		int WAGE_PER_HOUR = 20;
+		int FULL_TIME = 8;
+		int dailyWage = 0;
+		double empCheck = Math.floor(Math.random() * 10) % 3;
+		if (empCheck == IS_PRESENT) {
+			System.out.println("Employee is present");
+			dailyWage = WAGE_PER_HOUR * FULL_TIME;
+		}
+
+		else {
+			System.out.println("Employee is absent");
+		}
+		System.out.println("Emp Wage: " + dailyWage);
 
 	}
 }
