01054ff3a6ab308232a3e1ff1cc1d84653e02880
krishna319 <km83843@gmail.com>
2021-08-05 18:43:08 +0530

    Added:Number of Working Days

--- src/wageofemployee/EmployeeFee.java
+++ src/wageofemployee/EmployeeFee.java
@@ -6,24 +6,27 @@ public class EmployeeFee {
 		final int IS_PRESENT = 1;
 		final int IS_PART_TIME_PRESENT = 2;
 		final int WAGE_PER_HOUR = 20;
+		final int NUM_OF_WORKING_DAYS = 20;
 
 		int empHrs = 0;
 		int empWage = 0;
-		int empCheck = (int) Math.floor(Math.random() * 10) % 3;
-		switch (empCheck) {
-		case IS_PRESENT:
-			empHrs = 8;
-			System.out.println("Employee is present");
-			break;
-		case IS_PART_TIME_PRESENT:
-			empHrs = 4;
-			System.out.println("Employe is part time present");
-			break;
-		default:
-			empHrs = 0;
+		int totalEmpWage = 0;
+		for (int day = 0; day < NUM_OF_WORKING_DAYS; day++) {
+			int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+			switch (empCheck) {
+			case IS_PRESENT:
+				empHrs = 8;
+				break;
+			case IS_PART_TIME_PRESENT:
+				empHrs = 4;
+				break;
+			default:
+				empHrs = 0;
+			}
+			empWage = empHrs * WAGE_PER_HOUR;
+			totalEmpWage += empWage;
 		}
-		empWage = empHrs * WAGE_PER_HOUR;
-		System.out.println("Emp Wage: " + empWage);
+		System.out.println("Total Emp Wage:" + totalEmpWage);
 
 	}
 }
