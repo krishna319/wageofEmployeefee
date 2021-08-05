fe2f4aed434ab6d4b7964191d8f86561b0655503
krishna319 <km83843@gmail.com>
2021-08-05 18:28:03 +0530

    Added: Case Statements

--- src/wageofemployee/EmployeeFee.java
+++ src/wageofemployee/EmployeeFee.java
@@ -3,24 +3,27 @@ package wageofemployee;
 public class EmployeeFee {
 	public static void main(String[] args) {
 
-		int IS_PRESENT = 1;
-		int IS_PART_TIME_PRESENT =2;
-		int WAGE_PER_HOUR = 20;
-		int FULL_TIME = 8;
-		int PART_TIME = 4;
-		int dailyWage = 0;
-		double empCheck = Math.floor(Math.random() * 10) % 3;
-		if (empCheck == IS_PRESENT) {
-			System.out.println("Employee is present");
-			dailyWage = WAGE_PER_HOUR * FULL_TIME;
-		} else if (empCheck == IS_PART_TIME_PRESENT) {
-			System.out.println("Employe is part time present");
-			dailyWage = WAGE_PER_HOUR * PART_TIME;
+		final int IS_PRESENT = 1;
+		final int IS_PART_TIME_PRESENT = 2;
+		final int WAGE_PER_HOUR = 20;
 
-		} else {
-			System.out.println("Employee is absent");
+		int empHrs = 0;
+		int empWage = 0;
+		int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+		switch (empCheck) {
+		case IS_PRESENT:
+			empHrs = 8;
+			System.out.println("Employee is present");
+			break;
+		case IS_PART_TIME_PRESENT:
+			empHrs = 4;
+			System.out.println("Employe is part time present");
+			break;
+		default:
+			empHrs = 0;
 		}
-		System.out.println("Emp Wage: " + dailyWage);
+		empWage = empHrs * WAGE_PER_HOUR;
+		System.out.println("Emp Wage: " + empWage);
 
 	}
 }
