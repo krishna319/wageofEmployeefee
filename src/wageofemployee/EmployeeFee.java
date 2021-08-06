038fe6b3a2ef6892f7069ceef697acb679058596
krishna319 <km83843@gmail.com>
2021-08-06 16:25:41 +0530

    Added:EmployeeMaxHrs

--- src/wageofemployee/EmployeeFee.java
+++ src/wageofemployee/EmployeeFee.java
@@ -1,17 +1,21 @@
 package wageofemployee;
 
 public class EmployeeFee {
+
+	public static final int IS_PRESENT = 1;
+	public static final int IS_PART_TIME_PRESENT = 2;
+	public static final int WAGE_PER_HOUR = 20;
+	public static final int NUM_OF_WORKING_DAYS = 20;
+	public static final int MAX_HRS_IN_MONTH = 10;
+
 	public static void main(String[] args) {
 
-		final int IS_PRESENT = 1;
-		final int IS_PART_TIME_PRESENT = 2;
-		final int WAGE_PER_HOUR = 20;
-		final int NUM_OF_WORKING_DAYS = 20;
-
 		int empHrs = 0;
-		int empWage = 0;
-		int totalEmpWage = 0;
-		for (int day = 0; day < NUM_OF_WORKING_DAYS; day++) {
+		int totalEmpHrs = 0;
+		int totalWorkingDays = 0;
+		while (totalEmpHrs <= MAX_HRS_IN_MONTH
+				&& totalWorkingDays < NUM_OF_WORKING_DAYS) {
+			totalWorkingDays++;
 			int empCheck = (int) Math.floor(Math.random() * 10) % 3;
 			switch (empCheck) {
 			case IS_PRESENT:
@@ -23,9 +27,11 @@ public class EmployeeFee {
 			default:
 				empHrs = 0;
 			}
-			empWage = empHrs * WAGE_PER_HOUR;
-			totalEmpWage += empWage;
+			totalEmpHrs += empHrs;
+			System.out
+					.println("day: " + totalWorkingDays + "Emp Hr: " + empHrs);
 		}
+		int totalEmpWage = totalEmpHrs * WAGE_PER_HOUR;
 		System.out.println("Total Emp Wage:" + totalEmpWage);
 
 	}
